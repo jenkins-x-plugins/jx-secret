f133237cdf0cb2078721c78f1557ff4fc0ead330
James Strachan <james.strachan@gmail.com>
2020-08-30 11:09:08 +0100

    fix: for local secret population
    Signed-off-by: James Strachan <james.strachan@gmail.com>

--- pkg/extsecrets/editor/local/local_editor_test.go
+++ pkg/extsecrets/editor/local/local_editor_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jenkins-x/jx-helpers/pkg/testhelpers"
 	v1 "github.com/jenkins-x/jx-secret/pkg/apis/external/v1"
+	"github.com/jenkins-x/jx-secret/pkg/extsecrets"
 	"github.com/jenkins-x/jx-secret/pkg/extsecrets/editor"
 	"github.com/jenkins-x/jx-secret/pkg/extsecrets/editor/local"
 	"github.com/stretchr/testify/require"
@@ -21,12 +22,34 @@ func TestLocalEditor(t *testing.T) {
 	propertyName := "token"
 	expectedPropertyValue := "someTokenValue"
 
+	labelName := "mylabel"
+	expetedLabelValue := "myLabelValue"
+
+	annotationName := "myannotation"
+	expectedAnnotationValue := "myAnnotationValue"
+
+	expectedSchemaAnnotation := `{"name":"` + secretName + `","properties":[{"name":"username","question":"bucket repository user name","help":"The username used to authenticate with the bucket repository","defaultValue":"admin"},{"name":"password","question":"bucket repository password","help":"The password to authenticate with the bucket repository","minLength":5,"maxLength":41,"generator":"password"}]}`
+
 	extSecret := &v1.ExternalSecret{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      secretName,
-			Namespace: ns,
+			Name:        secretName,
+			Namespace:   ns,
+			Annotations: map[string]string{},
 		},
-		Spec:   v1.ExternalSecretSpec{},
+		Spec: v1.ExternalSecretSpec{
+			Template: v1.Template{
+				Type: "MyType",
+				Metadata: metav1.ObjectMeta{
+					Labels: map[string]string{
+						labelName: expetedLabelValue,
+					},
+					Annotations: map[string]string{
+						annotationName:                    expectedAnnotationValue,
+						extsecrets.SchemaObjectAnnotation: expectedSchemaAnnotation,
+					},
+				},
+			},
+		},
 		Status: nil,
 	}
 	secretEditor, err := local.NewEditor(kubeClient, extSecret)
@@ -45,4 +68,8 @@ func TestLocalEditor(t *testing.T) {
 
 	secret, message := testhelpers.RequireSecretExists(t, kubeClient, ns, secretName)
 	testhelpers.AssertSecretEntryEquals(t, secret, propertyName, expectedPropertyValue, message)
+
+	testhelpers.AssertLabel(t, labelName, expetedLabelValue, secret.ObjectMeta, message)
+	testhelpers.AssertAnnotation(t, annotationName, expectedAnnotationValue, secret.ObjectMeta, message)
+	testhelpers.AssertAnnotation(t, extsecrets.SchemaObjectAnnotation, expectedSchemaAnnotation, secret.ObjectMeta, message)
 }
