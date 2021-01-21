5261a55e679b929d5f55ff385c01461008e78fbb
James Strachan <james.strachan@gmail.com>
2021-01-21 06:26:49 +0000

    chore: disable auto promote to jx-cli

--- promote.sh
+++ promote.sh
@@ -2,4 +2,5 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-jx step create pr regex --regex '\s+SecretVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git
+# TODO - lets disable promoting the new jx-secret release to jx-cli until we've got the new secret facade code working
+# jx step create pr regex --regex '\s+SecretVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git
