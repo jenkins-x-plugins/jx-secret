808f4b92fafbd53718b7bead9dfa9c1094fe3db9
James Strachan <james.strachan@gmail.com>
2021-01-25 12:46:07 +0000

    fix: re-enable promotion to jx-cli

--- promote.sh
+++ promote.sh
@@ -2,5 +2,4 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-# TODO - lets disable promoting the new jx-secret release to jx-cli until we've got the new secret facade code working
-# jx step create pr regex --regex '\s+SecretVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git
+jx step create pr regex --regex '\s+SecretVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git
