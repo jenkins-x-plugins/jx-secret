e3d48dace7fccb052218c752dde2320ff0e9b567
James Strachan <james.strachan@gmail.com>
2021-01-25 12:56:32 +0000

    fix: add udpatebot config

--- promote.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "promoting the new version ${VERSION} to downstream repositories"
-
-jx step create pr regex --regex '\s+SecretVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git
