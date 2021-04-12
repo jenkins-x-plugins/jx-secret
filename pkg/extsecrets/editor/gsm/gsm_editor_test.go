8128f23f03808706e9b301e8d15705fa21131d57
James Rawlings <rawlingsj80@gmail.com>
2021-04-12 14:41:14 +0100

    chore: add unit test to make sure we can still update GSM secrets without a JSON payload

--- pkg/extsecrets/editor/gsm/gsm_editor_test.go
+++ pkg/extsecrets/editor/gsm/gsm_editor_test.go
@@ -42,3 +42,30 @@ func Test_client_Write(t *testing.T) {
 
 	assert.Equal(t, expected, results, "json file should contain key/value pairs of secret properties")
 }
+
+func Test_client_WriteSingle(t *testing.T) {
+
+	file, err := ioutil.TempFile("", "jx")
+	assert.NoError(t, err, "should not error creating a temporary file")
+
+	c := &client{}
+
+	p := &editor.KeyProperties{
+		Properties: []editor.PropertyValue{
+			{
+				Value: "bar",
+			},
+		},
+	}
+	existingSecrets := make(map[string]string)
+	err = c.writeTemporarySecretPropertiesJSON(existingSecrets, p, file)
+	assert.NoError(t, err)
+
+	expected, err := ioutil.ReadFile(filepath.Join("test_data", "write_single", "expected.json"))
+	assert.NoError(t, err, "shouldn't fail to read expected results file")
+
+	results, err := ioutil.ReadFile(file.Name())
+	assert.NoError(t, err, "shouldn't fail to read results file")
+
+	assert.Equal(t, expected, results, "json file should contain key/value pairs of secret properties")
+}
