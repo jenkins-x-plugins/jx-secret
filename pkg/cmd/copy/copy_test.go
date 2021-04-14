27580cbf982ac90aa62cd046e784e3c265575055
James Strachan <james.strachan@gmail.com>
2021-04-14 12:41:24 +0100

    fix: allow copy of secrets by name
    Signed-off-by: James Strachan <james.strachan@gmail.com>

--- pkg/cmd/copy/copy_test.go
+++ pkg/cmd/copy/copy_test.go
@@ -14,9 +14,11 @@ import (
 	"k8s.io/client-go/kubernetes/fake"
 )
 
-func TestCopy(t *testing.T) {
-	ns := "jx"
+var (
+	ns = "jx"
+)
 
+func TestCopyBySelector(t *testing.T) {
 	kubeObjects := []runtime.Object{
 		&corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{
@@ -70,4 +72,76 @@ func TestCopy(t *testing.T) {
 	require.NoError(t, err, "should have found to namespace %s", o.ToNamespace)
 	require.NotNil(t, tons, "should have to namespace %s", o.ToNamespace)
 	assert.Equal(t, o.ToNamespace, tons.Name, "toNamespace.Name")
+}
+
+func TestCopyByName(t *testing.T) {
+	kubeObjects := []runtime.Object{
+		&corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "lighthouse-oauth-token",
+				Namespace: ns,
+				Labels: map[string]string{
+					"beer": "stella",
+				},
+			},
+			Data: map[string][]byte{
+				"oauth": []byte("dummyPipelineUserToken"),
+			},
+		},
+		&corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "knative-docker-user-pass",
+				Namespace: ns,
+				Labels: map[string]string{
+					"beer": "stella",
+				},
+			},
+			Data: map[string][]byte{
+				"username": []byte("dummyDockerUsername"),
+				"password": []byte("dummyDockerPassword"),
+			},
+		},
+		&corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "lighthouse-hmac-token",
+				Namespace: ns,
+			},
+			Data: map[string][]byte{
+				"oauth": []byte("dummyPipelineUserToken"),
+			},
+		},
+	}
+	_, o := copy.NewCmdCopy()
+	o.Namespace = ns
+	o.KubeClient = fake.NewSimpleClientset(kubeObjects...)
+	o.ToNamespace = "my-preview-env"
+	o.Name = "lighthouse-oauth-token"
+	o.CreateNamespace = true
+	err := o.Run()
+	require.NoError(t, err, "failed to run export")
+
+	secret, message := testhelpers.RequireSecretExists(t, o.KubeClient, o.ToNamespace, "lighthouse-oauth-token")
+	testhelpers.AssertSecretEntryEquals(t, secret, "oauth", "dummyPipelineUserToken", message)
+
+	ctx := context.TODO()
+	tons, err := o.KubeClient.CoreV1().Namespaces().Get(ctx, o.ToNamespace, metav1.GetOptions{})
+	require.NoError(t, err, "should have found to namespace %s", o.ToNamespace)
+	require.NotNil(t, tons, "should have to namespace %s", o.ToNamespace)
+	assert.Equal(t, o.ToNamespace, tons.Name, "toNamespace.Name")
+
+	secretList, err := o.KubeClient.CoreV1().Secrets(o.ToNamespace).List(ctx, metav1.ListOptions{})
+	require.NoError(t, err, "failed to list secrets in namespace %s", o.ToNamespace)
+	require.NotNil(t, secretList, "should have SecretList from namespace %s", o.ToNamespace)
+	assert.Len(t, secretList.Items, 1, "should have 1 secret in the namespace %s", o.ToNamespace)
+}
+
+func TestCopyNoSelectorOrNameFails(t *testing.T) {
+	_, o := copy.NewCmdCopy()
+	o.Namespace = ns
+	o.KubeClient = fake.NewSimpleClientset()
+
+	o.ToNamespace = "my-preview-env"
+	o.CreateNamespace = true
+	err := o.Run()
+	require.Error(t, err, "should have failed")
 }
