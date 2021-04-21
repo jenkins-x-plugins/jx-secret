f8680584eccda3ec3d40cc9da83c906a2b78fa8e
James Strachan <james.strachan@gmail.com>
2021-04-21 09:41:36 +0100

    chore: add more unit tests of the different backends
    Signed-off-by: James Strachan <james.strachan@gmail.com>

--- pkg/cmd/convert/convert_test.go
+++ pkg/cmd/convert/convert_test.go
@@ -20,6 +20,11 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
+var (
+	// generateTestOutput enable to regenerate the expected output
+	generateTestOutput = false
+)
+
 func TestToExtSecrets(t *testing.T) {
 	sourceData := filepath.Join("test_data", "simple")
 	fileNames, err := ioutil.ReadDir(sourceData)
@@ -223,6 +228,338 @@ func TestMultipleBackendTypes(t *testing.T) {
 
 		result := strings.TrimSpace(string(resultData))
 		expectedText := strings.TrimSpace(string(expectData))
+		if d := cmp.Diff(result, expectedText); d != "" {
+			t.Errorf("Generated external secret for file %s did not match expected: %s", tc.SourceFile, d)
+		}
+		t.Logf("generated for file %s file\n%s\n", tc.SourceFile, result)
+	}
+}
+
+func TestAlicloud(t *testing.T) {
+	sourceData := filepath.Join("test_data", "alicloud")
+	fileNames, err := ioutil.ReadDir(sourceData)
+	assert.NoError(t, err)
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "could not create temp dir")
+
+	type testCase struct {
+		SourceFile   string
+		ResultFile   string
+		ExpectedFile string
+	}
+
+	var testCases []testCase
+	for _, f := range fileNames {
+		if !f.IsDir() {
+			continue
+		}
+		name := f.Name()
+		if name == ".jx" {
+			continue
+		}
+		srcFile := filepath.Join(sourceData, name, "source.yaml")
+		expectedFile := filepath.Join(sourceData, name, "expected.yaml")
+		require.FileExists(t, srcFile)
+		require.FileExists(t, expectedFile)
+
+		outFile := filepath.Join(tmpDir, name+".yaml")
+		err = files.CopyFile(srcFile, outFile)
+		require.NoError(t, err, "failed to copy %s to %s", srcFile, outFile)
+
+		testCases = append(testCases, testCase{
+			SourceFile:   srcFile,
+			ResultFile:   outFile,
+			ExpectedFile: expectedFile,
+		})
+	}
+
+	_, eo := convert.NewCmdSecretConvert()
+	eo.Dir = tmpDir
+	eo.HelmSecretFolder = filepath.Join(tmpDir, "helm-secrets")
+	eo.SourceDir = tmpDir
+
+	eo.SecretMapping, _, err = secretmapping.LoadSecretMapping(sourceData, true)
+	require.NoError(t, err, "failed to load secret mapping")
+
+	assert.Equal(t, v1alpha1.BackendTypeAlicloud, eo.SecretMapping.Spec.Secrets[0].BackendType)
+	assert.Equal(t, v1alpha1.BackendTypeAlicloud, eo.SecretMapping.Spec.Secrets[1].BackendType)
+	assert.Equal(t, v1alpha1.BackendTypeAlicloud, eo.SecretMapping.Spec.Secrets[2].BackendType)
+
+	err = eo.Run()
+	require.NoError(t, err, "failed to convert to external secrets in dir %s", tmpDir)
+
+	for _, tc := range testCases {
+		if generateTestOutput {
+			generatedFile := tc.ResultFile
+			expectedPath := tc.ExpectedFile
+			data, err := ioutil.ReadFile(generatedFile)
+			require.NoError(t, err, "failed to load %s", generatedFile)
+
+			err = ioutil.WriteFile(expectedPath, data, 0666)
+			require.NoError(t, err, "failed to save file %s", expectedPath)
+
+			t.Logf("saved file %s\n", expectedPath)
+			continue
+		}
+
+		resultData, err := ioutil.ReadFile(tc.ResultFile)
+		require.NoError(t, err, "failed to load results %s", tc.ResultFile)
+		expectData, err := ioutil.ReadFile(tc.ExpectedFile)
+		require.NoError(t, err, "failed to load results %s", tc.ExpectedFile)
+
+		result := strings.TrimSpace(string(resultData))
+		expectedText := strings.TrimSpace(string(expectData))
+
+		if d := cmp.Diff(result, expectedText); d != "" {
+			t.Errorf("Generated external secret for file %s did not match expected: %s", tc.SourceFile, d)
+		}
+		t.Logf("generated for file %s file\n%s\n", tc.SourceFile, result)
+	}
+}
+
+func TestAWSSecretsManager(t *testing.T) {
+	sourceData := filepath.Join("test_data", "awsSecretsManager")
+	fileNames, err := ioutil.ReadDir(sourceData)
+	assert.NoError(t, err)
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "could not create temp dir")
+
+	type testCase struct {
+		SourceFile   string
+		ResultFile   string
+		ExpectedFile string
+	}
+
+	var testCases []testCase
+	for _, f := range fileNames {
+		if !f.IsDir() {
+			continue
+		}
+		name := f.Name()
+		if name == ".jx" {
+			continue
+		}
+		srcFile := filepath.Join(sourceData, name, "source.yaml")
+		expectedFile := filepath.Join(sourceData, name, "expected.yaml")
+		require.FileExists(t, srcFile)
+		require.FileExists(t, expectedFile)
+
+		outFile := filepath.Join(tmpDir, name+".yaml")
+		err = files.CopyFile(srcFile, outFile)
+		require.NoError(t, err, "failed to copy %s to %s", srcFile, outFile)
+
+		testCases = append(testCases, testCase{
+			SourceFile:   srcFile,
+			ResultFile:   outFile,
+			ExpectedFile: expectedFile,
+		})
+	}
+
+	_, eo := convert.NewCmdSecretConvert()
+	eo.Dir = tmpDir
+	eo.HelmSecretFolder = filepath.Join(tmpDir, "helm-secrets")
+	eo.SourceDir = tmpDir
+
+	eo.SecretMapping, _, err = secretmapping.LoadSecretMapping(sourceData, true)
+	require.NoError(t, err, "failed to load secret mapping")
+
+	assert.Equal(t, v1alpha1.BackendTypeAWSSecretsManager, eo.SecretMapping.Spec.Secrets[0].BackendType)
+	assert.Equal(t, v1alpha1.BackendTypeAWSSecretsManager, eo.SecretMapping.Spec.Secrets[1].BackendType)
+	assert.Equal(t, v1alpha1.BackendTypeAWSSecretsManager, eo.SecretMapping.Spec.Secrets[2].BackendType)
+
+	err = eo.Run()
+	require.NoError(t, err, "failed to convert to external secrets in dir %s", tmpDir)
+
+	for _, tc := range testCases {
+		if generateTestOutput {
+			generatedFile := tc.ResultFile
+			expectedPath := tc.ExpectedFile
+			data, err := ioutil.ReadFile(generatedFile)
+			require.NoError(t, err, "failed to load %s", generatedFile)
+
+			err = ioutil.WriteFile(expectedPath, data, 0666)
+			require.NoError(t, err, "failed to save file %s", expectedPath)
+
+			t.Logf("saved file %s\n", expectedPath)
+			continue
+		}
+
+		resultData, err := ioutil.ReadFile(tc.ResultFile)
+		require.NoError(t, err, "failed to load results %s", tc.ResultFile)
+		expectData, err := ioutil.ReadFile(tc.ExpectedFile)
+		require.NoError(t, err, "failed to load results %s", tc.ExpectedFile)
+
+		result := strings.TrimSpace(string(resultData))
+		expectedText := strings.TrimSpace(string(expectData))
+
+		if d := cmp.Diff(result, expectedText); d != "" {
+			t.Errorf("Generated external secret for file %s did not match expected: %s", tc.SourceFile, d)
+		}
+		t.Logf("generated for file %s file\n%s\n", tc.SourceFile, result)
+	}
+}
+
+func TestAWSParameterStore(t *testing.T) {
+	sourceData := filepath.Join("test_data", "awsParameterStore")
+	fileNames, err := ioutil.ReadDir(sourceData)
+	assert.NoError(t, err)
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "could not create temp dir")
+
+	type testCase struct {
+		SourceFile   string
+		ResultFile   string
+		ExpectedFile string
+	}
+
+	var testCases []testCase
+	for _, f := range fileNames {
+		if !f.IsDir() {
+			continue
+		}
+		name := f.Name()
+		if name == ".jx" {
+			continue
+		}
+		srcFile := filepath.Join(sourceData, name, "source.yaml")
+		expectedFile := filepath.Join(sourceData, name, "expected.yaml")
+		require.FileExists(t, srcFile)
+		require.FileExists(t, expectedFile)
+
+		outFile := filepath.Join(tmpDir, name+".yaml")
+		err = files.CopyFile(srcFile, outFile)
+		require.NoError(t, err, "failed to copy %s to %s", srcFile, outFile)
+
+		testCases = append(testCases, testCase{
+			SourceFile:   srcFile,
+			ResultFile:   outFile,
+			ExpectedFile: expectedFile,
+		})
+	}
+
+	_, eo := convert.NewCmdSecretConvert()
+	eo.Dir = tmpDir
+	eo.HelmSecretFolder = filepath.Join(tmpDir, "helm-secrets")
+	eo.SourceDir = tmpDir
+
+	eo.SecretMapping, _, err = secretmapping.LoadSecretMapping(sourceData, true)
+	require.NoError(t, err, "failed to load secret mapping")
+
+	assert.Equal(t, v1alpha1.BackendTypeAWSParameterStore, eo.SecretMapping.Spec.Secrets[0].BackendType)
+	assert.Equal(t, v1alpha1.BackendTypeAWSParameterStore, eo.SecretMapping.Spec.Secrets[1].BackendType)
+	assert.Equal(t, v1alpha1.BackendTypeAWSParameterStore, eo.SecretMapping.Spec.Secrets[2].BackendType)
+
+	err = eo.Run()
+	require.NoError(t, err, "failed to convert to external secrets in dir %s", tmpDir)
+
+	for _, tc := range testCases {
+		if generateTestOutput {
+			generatedFile := tc.ResultFile
+			expectedPath := tc.ExpectedFile
+			data, err := ioutil.ReadFile(generatedFile)
+			require.NoError(t, err, "failed to load %s", generatedFile)
+
+			err = ioutil.WriteFile(expectedPath, data, 0666)
+			require.NoError(t, err, "failed to save file %s", expectedPath)
+
+			t.Logf("saved file %s\n", expectedPath)
+			continue
+		}
+
+		resultData, err := ioutil.ReadFile(tc.ResultFile)
+		require.NoError(t, err, "failed to load results %s", tc.ResultFile)
+		expectData, err := ioutil.ReadFile(tc.ExpectedFile)
+		require.NoError(t, err, "failed to load results %s", tc.ExpectedFile)
+
+		result := strings.TrimSpace(string(resultData))
+		expectedText := strings.TrimSpace(string(expectData))
+
+		if d := cmp.Diff(result, expectedText); d != "" {
+			t.Errorf("Generated external secret for file %s did not match expected: %s", tc.SourceFile, d)
+		}
+		t.Logf("generated for file %s file\n%s\n", tc.SourceFile, result)
+	}
+}
+
+func TestIBMSecretsManager(t *testing.T) {
+	sourceData := filepath.Join("test_data", "ibmSecretsManager")
+	fileNames, err := ioutil.ReadDir(sourceData)
+	assert.NoError(t, err)
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "could not create temp dir")
+
+	type testCase struct {
+		SourceFile   string
+		ResultFile   string
+		ExpectedFile string
+	}
+
+	var testCases []testCase
+	for _, f := range fileNames {
+		if !f.IsDir() {
+			continue
+		}
+		name := f.Name()
+		if name == ".jx" {
+			continue
+		}
+		srcFile := filepath.Join(sourceData, name, "source.yaml")
+		expectedFile := filepath.Join(sourceData, name, "expected.yaml")
+		require.FileExists(t, srcFile)
+		require.FileExists(t, expectedFile)
+
+		outFile := filepath.Join(tmpDir, name+".yaml")
+		err = files.CopyFile(srcFile, outFile)
+		require.NoError(t, err, "failed to copy %s to %s", srcFile, outFile)
+
+		testCases = append(testCases, testCase{
+			SourceFile:   srcFile,
+			ResultFile:   outFile,
+			ExpectedFile: expectedFile,
+		})
+	}
+
+	_, eo := convert.NewCmdSecretConvert()
+	eo.Dir = tmpDir
+	eo.HelmSecretFolder = filepath.Join(tmpDir, "helm-secrets")
+	eo.SourceDir = tmpDir
+
+	eo.SecretMapping, _, err = secretmapping.LoadSecretMapping(sourceData, true)
+	require.NoError(t, err, "failed to load secret mapping")
+
+	assert.Equal(t, v1alpha1.BackendTypeIBMSecretsManager, eo.SecretMapping.Spec.Secrets[0].BackendType)
+	assert.Equal(t, v1alpha1.BackendTypeIBMSecretsManager, eo.SecretMapping.Spec.Secrets[1].BackendType)
+	assert.Equal(t, v1alpha1.BackendTypeIBMSecretsManager, eo.SecretMapping.Spec.Secrets[2].BackendType)
+
+	err = eo.Run()
+	require.NoError(t, err, "failed to convert to external secrets in dir %s", tmpDir)
+
+	for _, tc := range testCases {
+		if generateTestOutput {
+			generatedFile := tc.ResultFile
+			expectedPath := tc.ExpectedFile
+			data, err := ioutil.ReadFile(generatedFile)
+			require.NoError(t, err, "failed to load %s", generatedFile)
+
+			err = ioutil.WriteFile(expectedPath, data, 0666)
+			require.NoError(t, err, "failed to save file %s", expectedPath)
+
+			t.Logf("saved file %s\n", expectedPath)
+			continue
+		}
+
+		resultData, err := ioutil.ReadFile(tc.ResultFile)
+		require.NoError(t, err, "failed to load results %s", tc.ResultFile)
+		expectData, err := ioutil.ReadFile(tc.ExpectedFile)
+		require.NoError(t, err, "failed to load results %s", tc.ExpectedFile)
+
+		result := strings.TrimSpace(string(resultData))
+		expectedText := strings.TrimSpace(string(expectData))
+
 		if d := cmp.Diff(result, expectedText); d != "" {
 			t.Errorf("Generated external secret for file %s did not match expected: %s", tc.SourceFile, d)
 		}
